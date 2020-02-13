3f2ead0d5ca351c22e5a5a5873ad51f962c6bc58
Jonathan C. Narvaez Martinez <jnarvaez@zeustecnologia.com>
2020-02-13 16:10:39 -0500

    chat

---
+++ src/components/ChatBox.js
@@ -0,0 +1,92 @@
+import React, {useState, useEffect, useRef} from 'react'
+//import EmojiPicker from 'emoji-picker-react';
+import Messages from './Messages'
+import './chatStyle.css'
+
+const styleFocus = {
+    border: 'solid 1px #127ff7'
+}
+const styleBlur = {
+    border: 'solid 1px #CCC'
+}
+const ChatBox = (props) => {
+
+    const {userActual} = props
+    const [message, setMessage] = useState([])
+    const [onFocus, setOnFocus] = useState(false)
+    const [messages, setMessages] = useState([])
+    //const [users, setUsers] = useState([])
+    const txtMessage = useRef()
+    const chatFocused = useRef()
+    const [date, setDate] = useState('')
+
+    useEffect(() => {
+        document.addEventListener('focus', handleFocus)
+        return () =>{
+            document.removeEventListener('focus',handleFocus)
+        }
+    },[])
+    
+    /*const myCallback = (code) =>{
+        console.log(code)
+        //const emoji = String.fromCodePoint(`0x${code}`)
+        //setMessage(message => [...message, emoji])
+        //txtMessage.value += ` ${emoji}`
+        //console.log(emoji)
+    }*/
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        setDate(new Date().toDateString())
+        setMessages(messages => [...messages, message])
+        setMessage({text: ''})
+    }
+    const handleChange = (e) => {
+        e.preventDefault()
+        let message = e.target.value
+        setMessage({text: message, send: true})
+    }
+    const handleFocus = () =>{
+        setOnFocus(true)
+    }
+
+    return(
+        <div className="chatBox">
+            <div className="chat"
+                onClick={handleFocus}
+                style={onFocus ? styleFocus : styleBlur}
+                ref={chatFocused}
+            >
+            {messages.map((message,index) =>
+                <div key={index}>
+                    <Messages
+                        send={message.send}
+                        userActual={userActual}
+                        message={message.text}
+                        date={date}
+                    />
+                </div>
+            )
+            }
+            </div>
+            <div className="chatInput">
+                <form
+                    onSubmit={handleSubmit}
+                >
+                    <input
+                        type='text'
+                        name='message'
+                        value={message.text}
+                        onChange={handleChange}
+                        ref={txtMessage}
+                        placeholder='Type your message'
+                    />
+                    <button type='submit'>
+                        Send
+                    </button>
+                </form>
+            </div>
+        </div>
+    )
+}
+export default ChatBox
---
+++ src/components/Messages.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Messages = (props) => {
+    const {send, userActual, message, date} = props
+    let styleClass = '-message'
+    if(send)
+        styleClass = `${styleClass}-send`
+
+    return(
+        <div>
+            <div className={`dv${styleClass}`}>
+                    <label className={`lbl${styleClass}`}>
+                        {userActual} (yo)
+                    </label>
+                    <div className={`img${styleClass}`}></div>
+                    <p className={`text${styleClass}`}>
+                        {message}
+                    </p>
+                    <p className={`lbl-date${styleClass}`}>
+                        {date}
+                    </p>
+                </div>
+                <hr/>
+        </div>
+    )
+}
+
+export default Messages
